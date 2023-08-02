b2fbbd3e228e356baf1494107ea3332a5b054294
JiseokSeo <tjwltjr69@gmail.com>
2023-08-02 16:13:26 +0900

    DOM으로 HTML 객체 제어하기 | 프런트 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,5 +1,18 @@
 'use strict'; // 로그인 기능 구현
 
-console.log('hello');
-console.log('bye')
-console.log('test')
+// DOM : html의 데이터를 가져와 자바스크립트로 다룰 수 있게 해주는 인터프리터의 일종
+
+const id = document.querySelector("#id"), // #: id 태그의 값을 가져옴
+    psword = document.querySelector('#psword'),
+    loginBtn = document.querySelector('button'); // 그냥 넣으면 tag 전체를 가져옴
+
+loginBtn.addEventListener('click', login);
+
+function login() {
+    console.log('로그인 버튼 클릭');
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+    console.log(req);
+};
